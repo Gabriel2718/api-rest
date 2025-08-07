53ad7fda6e7f6b5a253e009b396eb1afcc0261f0
Gabriel2718 <gcr2@outlook.com.br>
2025-08-07 15:01:49 -0300

    PostgreSQL integration

--- api-rest/src/main/java/com/gabriel2718/api_rest/controllers/ProductController.java
+++ api-rest/src/main/java/com/gabriel2718/api_rest/controllers/ProductController.java
@@ -1,14 +1,30 @@
 package com.gabriel2718.api_rest.controllers;
 
+import com.gabriel2718.api_rest.model.Product;
+import com.gabriel2718.api_rest.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+
+    @Autowired
+    ProductRepository repository;
+    //Injeção de um objeto da interface ProductRepository
+
     @GetMapping
-    public String hello() {
-        return "Hello";
+    public ResponseEntity getAll() {
+        List<Product> listProducts = repository.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(listProducts);
     }
+    //Método executado quando a aplicação recebe uma requisição do tipo Get no endpoint '/products'
 }
--- api-rest/src/main/java/com/gabriel2718/api_rest/repositories/ProductRepository.java
+++ api-rest/src/main/java/com/gabriel2718/api_rest/repositories/ProductRepository.java
@@ -4,5 +4,7 @@ import com.gabriel2718.api_rest.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+    //Uma interface que implementará métodos para a manipuação de dados do tipo Product,
+    //cujo identificador é do tipo Integer
 }
 
